c256eb1d554cba84f4fd0d469a43690963b29db1
denki307 <mbtdenki@gmail.com>
2025-05-10 08:53:33 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -171,10 +171,10 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             welcomeimg = welcomepic(
                 pic, user.first_name, member.chat.title, user.id, user.username
             )
-            button_text = "❍ 𓆩 𝗦𝐓𝐘𝐋𝐈𝐒𝐇 ⌯ 𝗡𝐀𝐌𝐄 𓆪 ❍"
+            button_text = "❍ 𓆩 𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏 𓆪 ❍"
             add_button_text = "❍ 𝐏𝐑𝐎𝐌𝐎𝐓𝐈𝐎𝐍 𝐀𝐕𝐀𝐈𝐋𝐀𝐁𝐋𝐄 ❍"
-            deep_link = f"https://t.me/TG_NAME_STYLE"
-            add_link = f"https://t.me/PROMOTION_UPDATE/51"
+            deep_link = f"https://t.me/JALLI_KATTU_KALA143"
+            add_link = f"https://t.me/MUSIC_BOT_WORLD"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
                 member.chat.id,
                 photo=welcomeimg,
